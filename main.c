ea078f3c39d291c6e84a59fb9a4e84a49e72440e
lcy8047 <lcy8047@gmail.com>
2023-05-08 19:51:39 +0900

    Update input and print num

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main( void )
 {
-	printf("Hello, World!\n");
+	int input_num;
+	int random_num;
+	
+	random_num = rand() % 1000;
+
+	printf("input : ");
+	scanf("%d", &input_num);
+
+	printf("your num : %d\n", input_num);
+	printf("random num : %d\n", random_num);
+	random_num = rand() % 1000;
+	printf("random num : %d\n", random_num);
+
 	return 0;
 }
