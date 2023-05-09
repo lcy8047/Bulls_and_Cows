9770becb6d2f2f7d595f3eac84ec10e20051ae52
lcy8047 <lcy8047@gmail.com>
2023-05-09 17:01:56 +0900

    Update input and print num solution

--- main.c
+++ main.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main( void )
 {
 	int input_num;
-	int random_num;
-	
-	random_num = rand() % 1000;
 
-	printf("input : ");
+	printf("Input 3 digits number : ");
 	scanf("%d", &input_num);
 
-	printf("your num : %d\n", input_num);
-	printf("random num : %d\n", random_num);
-	random_num = rand() % 1000;
-	printf("random num : %d\n", random_num);
+	printf("your number is %d\n", input_num);
 
 	return 0;
 }
